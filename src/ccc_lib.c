7a94b021562c1e564debe6c42bb547f3cf92cc31
mtiemi <marianetiemi@gmail.com>
2018-03-19 21:58:50 -0300

    Adicionei uns comentários

--- src/ccc_lib.c
+++ src/ccc_lib.c
@@ -1,66 +1,63 @@
 
 #include "ccc_lib.h"
 
-
-void camelcase_to_underline(char *input, char *output) {
-
-  int i = 0, j = 0;
+void camelcase_to_underline(char *input, char *output) 
+{
+    int i = 0, j = 0;
   
-  while(input[i] != '\0')
-  {
-    if(i == 0) // converte a primeira letra em minusculo
+    while(input[i] != '\0')
     {
-        output[j] = input[i] + 32;
-        j++;
-    }else
-    {
-        if(input[i] >= 65 && input[i] <= 90) //encontrando a letra maiscula no meio da string
+        if(i == 0) // converte a primeira letra em minusculo
         {
-            output[j] = 95; // troca a maiscula por underline
-            j++;
             output[j] = input[i] + 32;
             j++;
         }else
         {
-            output[j] = input[i];
-            j++;
+            if(input[i] >= 65 && input[i] <= 90) //encontrando a letra maiscula no meio da string
+            {
+                output[j] = 95; // troca a maiscula por underline
+                j++;
+                output[j] = input[i] + 32;
+                j++;
+            }else
+            {
+                output[j] = input[i];
+                j++;
+            }
         }
+        i++;
     }
-   i++;;
-  }
     
-  return;
+    return;
 }
 
 void underline_to_camelcase(char *input, char *output) 
 {
-
     int i = 0, j = 0;
-  
-  while(input[i] != '\0')
-  {
-    if(i == 0) // converte a primeira letra em maisculo
+    
+    while(input[i] != '\0')
     {
-        output[j] = input[i] - 32;
-        j++;
-    }else
-    {
-        if(input[i] == 95) //encontrando o underline
+        if(i == 0) // converte a primeira letra em maisculo
         {
-            i++; // le o proximo caractere
-            output[j] = input[i] - 32; //coloca a maiuscula
+            output[j] = input[i] - 32;
             j++;
-            
         }else
         {
-            output[j] = input[i];
-            j++;
+            if(input[i] == 95) //encontrando o underline
+            {
+                i++; // le o proximo caractere
+                output[j] = input[i] - 32; //coloca a maiuscula
+                j++;
+                
+            }else
+            {
+                output[j] = input[i];
+                j++;
+            }
         }
-    }
-    i++;
-  }
-  
-  return;
+        i++;
+    }      
+    return;
 }
 
 
